ccf69f65bce15e4d8e1fb4c15ee4c65fda3694b7
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-03-03 14:39:23 -0500

    Added a model to match the structure of the JSON object received from our endpoint.

---
+++ JokeFinder/Model/Joke.swift
@@ -0,0 +1,26 @@
+//
+//  Joke.swift
+//  JokeFinder
+//
+//  Created by Sebastian on 2025-03-03.
+//
+
+import Foundation
+
+struct Joke: Identifiable, Codable {
+    
+    // MARK: Stored properties
+    let type: String
+    let setup: String?
+    let punchline: String?
+    let id: Int
+        
+}
+ 
+// Create an example joke for testing purposes
+let exampleJoke = Joke(
+    type: "general",
+    setup: "Why couldn't the kid go to see the pirate movie?",
+    punchline: "Because it was rated arrrrr!",
+    id: 310
+)

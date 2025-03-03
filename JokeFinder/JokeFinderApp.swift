b7015c18193eefb053bfc4ff3f5aa1f22f70862b
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-03-03 14:28:36 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Sebastian on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
